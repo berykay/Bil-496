dbcbbc89fc895a5ab1fd4f6491962f44771cc377
Berkay Yıldız <berkayyildiz550@hotmail.com>
2024-01-15 22:54:43 +0300

    last changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import HomePage from "./pages/Homepage";
 import Header from "./pages/Header";
 
-
 export default function Home() {
   return (
       <main className={styles.main}>
